bea6cd702a1435e7e2261916573c436a1bd43209
JJonny <qweer.baranovsky@gmail.com>
2024-09-19 10:21:51 +0200

    Type hinting

--- app/services/tasks.py
+++ app/services/tasks.py
@@ -4,7 +4,7 @@ import uuid
 from celery import shared_task
 from celery.contrib.abortable import AbortableTask
 
-from app.data_access.models.models import Point
+from app.data_access.models.models import Distance, Point, PointAddress
 from app.data_access.task_manager import save_results_by_task_id
 from app.utils.file_processing import process_geo_data
 
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 def reverse_geocode_and_calculate_distances(points: list[Point], task_id: uuid.UUID):
     """Celery task for calculate distances and addresses for points."""
     try:
-        task_data = process_geo_data(points)
+        task_data: dict[str, [Distance | PointAddress]] = process_geo_data(points)
         save_results_by_task_id(task_id=task_id, task_data=task_data)
     except Exception as e:
         logger.error(f"An error occurred: {e}", exc_info=True)
